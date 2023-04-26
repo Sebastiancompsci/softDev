37ebbc4fdad6ab5f349c534e303ecbabbb4c85a2
Sebastian Ching <sching30@stuy.edu>
2023-04-26 09:37:15 -0400

    jadha

---
+++ 32_jsanim/animate.js
@@ -0,0 +1,73 @@
+var c = document.getElementById("playground");
+var dotButton = document.getElementById("buttonCircle");
+var dvdButton = document.getElementById("dvd");
+var stopButton = document.getElementById("buttonStop");
+
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "#0000FF";
+
+var requestID;
+
+var clear = (e) => {
+    e.preventDefault();
+    ctx.clearRect(0, 0, c.width, c.height);
+};
+
+var dvdLogoSetup =  function() {
+    window.cancelAnimationFrame(requestID);
+
+    var rectWidth = 600;
+    var rectHeight = 400;
+
+    var rectX = Math.random(c.width-rectWidth);
+    var rectY = Math.random(c.height- rectheight);
+
+    var xVel = 10 * Math.cos(Math.random() *2 * MathPI);
+    var yVel = 10 * Math.sin(Math.random() *2 * MathPI);
+
+    var logo = new Image();
+    logo.src = "logo_dvd.jpg";
+}
+
+var radius = 0;
+var growing  = true;
+
+
+let drawCirc = function() {
+    ctx.beginPath();
+    ctx.arc(c.width/2, c.height/2, radius, 0, Math.PI*2);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+var drawDot = () => {
+    clear();
+    if(radius==c.width/2){
+        growing = false;
+    }
+    else if(radius==0){
+        growing = true;
+    }
+
+    if (growing){
+        radius++;
+    }else{
+        radius--;
+     }
+     
+    drawCirc();
+
+    window.cancelAnimationFrame(requestID);
+    requestID = window.requestAnimationFrame(drawDot);
+}
+
+
+var stopIt = () => {
+    console.log("stopIt invoked...")
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
+}
+
+dotButton.addEventListener("click",drawDot);
+stopButton.addEventListener("click", stopIt);
